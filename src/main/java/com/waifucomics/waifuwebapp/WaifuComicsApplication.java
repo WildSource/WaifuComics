9fcf0a33d9b2a0cbdef4ab4a8f64693a6fa29a2c
WildSource <ilovetrap123@hotmail.com>
2022-08-02 00:43:10 -0400

    inital commit

---
+++ src/main/java/com/waifucomics/waifuwebapp/WaifuComicsApplication.java
@@ -0,0 +1,13 @@
+package com.waifucomics.waifuwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaifuComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaifuComicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waifucomics/waifuwebapp/WaifuComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waifucomics.waifuwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaifuComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
