168e9f69c509cd9e54e6d69a5bdd0cf56be1425b
WildSource <ilovetrap123@hotmail.com>
2022-10-18 16:23:20 -0400

    add security config but not working

--- src/main/java/com/waifucomics/waifuwebapp/views/LoginView.java
+++ src/main/java/com/waifucomics/waifuwebapp/views/LoginView.java
@@ -4,6 +4,7 @@ import com.waifucomics.waifuwebapp.entitites.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginView {
@@ -13,5 +14,10 @@ public class LoginView {
         Account account = new Account();
         model.addAttribute("loginInfo", account);
         return "login/login.html";
+    }
+
+    @PostMapping("/login")
+    String loginSuccessful() {
+        return "about/about.html";
     }
 }
